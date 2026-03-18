101453a1f9d08692d67d0b330cd88bc7dc49e711
phong <thanhphongchupanh@gmail.com>
2026-03-17 21:50:36 -0700

    update turbo version

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,14 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
+  },
 };
 
 export default withNextIntl(nextConfig);
