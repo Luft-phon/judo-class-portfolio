0a04e3abfb38bc1cb6e107f0a2e7a8afac68e469
phong <thanhphongchupanh@gmail.com>
2026-08-21 15:47:59 -0700

    resolve ci

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -3,9 +3,10 @@ import { Resend } from "resend";
 
 export const runtime = "edge"; // chạy nhanh trên Edge
 
-const resend = new Resend(process.env.RESEND_API_KEY!);
+
 
 export async function POST(req: Request) {
+  const resend = new Resend(process.env.RESEND_API_KEY!);
   try {
     const data = await req.json() as {
       fullname: string;
